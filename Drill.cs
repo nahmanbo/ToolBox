84b2ef7441e99edba3b8eb71f4c60dae95cab0c7
nahmanbo <nhmanbo2003@gmail.com>
2025-05-19 15:59:19 +0300

    add a drill class

---
+++ Drill.cs
@@ -0,0 +1,24 @@
+namespace ToolBox;
+
+public class Drill : Tool
+{
+    //====================================
+    //           Constructor
+    //====================================
+    public Drill() : base("Drill", "1.5kg")
+    {
+    }
+    
+    //====================================
+    //           Methods
+    //====================================
+    public override void Describe()
+    {
+        Console.WriteLine($"{_name} has {_weight} kg.");
+    }
+
+    public override void Use()
+    {
+        Console.WriteLine("The drill is currently in use");
+    }
+}
